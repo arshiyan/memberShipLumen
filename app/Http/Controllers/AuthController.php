ead683796b32a4264d75867f8c1c4578f2b2012b
javadarshiyan <dr.bloger@gmail.com>
2019-09-07 17:45:43 +0430

    add log hash for info in storage/log

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -118,9 +118,7 @@ class AuthController extends Controller
     protected function respondWithToken($token)
     {
         return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => \Auth::factory()->getTTL() * 60
+            'token' => $token
         ]);
     }
 }
